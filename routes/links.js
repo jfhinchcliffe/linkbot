cbd037fdf56f1c39eed5355841be20f9a0cb7826
jfhinchcliffe <hinchy@gmail.com>
2016-12-28 15:43:04 +1100

    finish add a link route

--- routes/links.js
+++ routes/links.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const Links = require('../models/link')
+const Link = require('../models/link')
 
-/* GET users listing. */
+// Show all Links
 router.get('/', function(req, res, next) {
-    Links.find()
+    Link.find()
     .then(links => {
         res.json(links)
     })
+});
+
+// Show 1 link based on URL
+router.get('/:id', function(req, res, next) {
+
+    Link.find(req.params.id)
+    .then(link => {
+        res.json(link)
+    })
+});
+
+// Create a link by posting to / route
+router.post('/', function(req, res, next) {
+    link = new Link;
+    link.url = req.body.url;
+    link.title = req.body.title;
+    link.votes = 0;
+    console.log(link);
+    link.save((err) => {
+    if (err) 
+      res.send(err);
+      res.json({status: "Link Added"});  
+    });
 });
 
 module.exports = router;
